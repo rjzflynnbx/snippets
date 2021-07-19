6707c16ced59aadbac575f3857c75f993df7a7cc
Richard Flynn <richard.flynn@boxever.com>
2021-07-19 18:36:18 +0700

    readme

--- decision_model_programmbles/snippets.js
+++ decision_model_programmbles/snippets.js
@@ -50,3 +50,15 @@ function getDataExtensionValue(guest, dataExtensionName, dataExtensionKey) {
 function getMapFromDataExtension(dataExtension) {
     return dataExtension.values;
 }
+
+
+function getNumberOfEvents(session, eventType) {
+    var numberOfEvents = 0;
+    for (var i = 0; i < session.events.length; i++) {
+        var event = session.events[i];
+        if (event.type === eventType) {
+            numberOfEvents++;
+        }
+    }
+    return numberOfEvents;
+}
