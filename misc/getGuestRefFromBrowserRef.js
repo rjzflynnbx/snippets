f1f2f8f8068e20d3298534c9d0fce1c89ac357c9
Richard Flynn <richard.flynn@boxever.com>
2021-08-10 16:09:44 +0700

    comments

--- misc/getGuestRefFromBrowserRef.js
+++ misc/getGuestRefFromBrowserRef.js
@@ -1,3 +1,4 @@
+// other Boxever JS lib methods: https://sitecore.cdpknowledgehub.com/docs/web-tagging-javascript-library-functions
 Boxever.browserShow(Boxever.getID(), 0, function(response){
     var guestRef = response.customer.ref;
     // Do something with guestRef
